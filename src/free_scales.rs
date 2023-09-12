1193ef0827603c8ae523748fa378b5cae2a02117
Jumpydh <marvin.graf@barifera.de>
2023-09-12 20:47:24 +0200

    Fixed playmode Added ScaleDown Added MidiInputCheck (Hot-plug not possible)

--- src/free_scales.rs
+++ src/free_scales.rs
@@ -1,31 +1,34 @@
-use std::sync::Arc;
+use std::{sync::Arc, time};
 
-use crate::{Ctx, get_midi_button, main_menu};
+use crate::{get_midi_down, Ctx};
 
 pub(crate) struct FreeScale;
 pub struct FsCtx<'a> {
     ctx: &'a mut Ctx,
-    scale: Vec<u8>,
+    scale: Vec<MidiNote>,
     offset: u8,
 }
 
 impl FreeScale {
     pub fn run(ctx: &mut Ctx) {
-        let scale = ScaleCreator::create_scale(ctx);
+        let scale = match ScaleCreator::create_scale(ctx) {
+            Ok(scale) => scale,
+            Err(_) => return,
+        };
         let mut fs_ctx = FsCtx {
             ctx,
             scale,
             offset: 0,
         };
-        Self::playing_scale(&mut fs_ctx);
+        Self::playing_scale_timedmode(&mut fs_ctx);
     }
 
-    fn playing_scale(ctx: &mut FsCtx) {
-        let mut input:Vec<u8> = vec![];
+    fn _playing_scale_waitmode(ctx: &mut FsCtx) {
+        let mut input: Vec<u8> = vec![];
         loop {
             if input.len() != ctx.scale.len() {
-                let message = get_midi_button(Arc::clone(&ctx.ctx.messages));
-                if ctx.scale[input.len()] + 21 + ctx.offset == message[1] {
+                let message = get_midi_down(Arc::clone(&ctx.ctx.messages), None);
+                if ctx.scale[input.len()].message[1] + 21 + ctx.offset == message[1] + 21 {
                     println!("Correct");
                     input.push(message[1]);
                 } else {
@@ -33,9 +36,33 @@ impl FreeScale {
                 }
                 println!(
                     "You played key: {}",
-                    get_note(&message[0], KEYS.clone())
+                    get_note(&(message[1] + 21), KEYS.clone())
                 );
             } else {
+                break;
+            }
+        }
+    }
+    fn playing_scale_timedmode(ctx: &mut FsCtx) {
+        let time = time::SystemTime::now();
+        println!("{:?}", time);
+        let mut input: Vec<u8> = vec![];
+        loop {
+            if input.len() != ctx.scale.len() {
+                let message = get_midi_down(Arc::clone(&ctx.ctx.messages), None);
+                if ctx.scale[input.len()].message[1] + 21 + ctx.offset == message[1] + 21 {
+                    println!("Correct");
+                    input.push(message[1]);
+                } else {
+                    println!("Wrong");
+                }
+                println!(
+                    "You played key: {}",
+                    get_note(&(message[1] + 21), KEYS.clone())
+                );
+            } else {
+                let time_end = time::SystemTime::now();
+                println!("{:?}", time_end.duration_since(time).unwrap().as_secs_f32());
                 break;
             }
         }
@@ -58,46 +85,59 @@ struct ScaleCreatorArgs {
 }
 
 impl ScaleCreator {
-    fn create_scale(ctx: &mut Ctx) -> Vec<u8> {
+    fn create_scale(ctx: &mut Ctx) -> Result<Vec<MidiNote>, ()> {
         let mut args = ScaleCreatorArgs {
             scale: Scale::Major,
             octaves: 1,
             offset: 0,
         };
-        Self::scale_selection(ctx, &mut args);
-        Self::octaves_selection(ctx, &mut args);
-        Self::startkey_selection(ctx, &mut args);
-        Self::build_notes(ctx, &mut args)
+        match Self::scale_selection(ctx, &mut args) {
+            Ok(_) => {}
+            Err(_) => return Err(()),
+        }
+        match Self::octaves_selection(ctx, &mut args) {
+            Ok(_) => {}
+            Err(_) => return Err(()),
+        }
+        match Self::startkey_selection(ctx, &mut args) {
+            Ok(_) => {}
+            Err(_) => return Err(()),
+        }
+        Ok(Self::build_notes(ctx, &mut args))
     }
-    fn scale_selection(ctx: &mut Ctx, args: &mut ScaleCreatorArgs) {
+    fn scale_selection(ctx: &mut Ctx, args: &mut ScaleCreatorArgs) -> Result<(), ()> {
         println!("Choose your Scale:");
         println!("Major: A");
         println!("Natural Minor: B");
         println!("Harmonic Minor: C");
         println!("Melodic Minor: D");
         println!("Press any other key to go back to the main menu");
-        let message = get_midi_button(Arc::clone(&ctx.messages));
+        let message = get_midi_down(Arc::clone(&ctx.messages), None);
         match message[1] {
             21 => {
                 println!("Playing major scale, press C!");
                 args.scale = Scale::Major;
+                Ok(())
             }
             23 => {
                 println!("Playing natural minor scale, press A!");
                 args.scale = Scale::NaturalMinor;
+                Ok(())
             }
             24 => {
                 println!("Playing harmonic minor scale, press A!");
                 args.scale = Scale::HarmonicMinor;
+                Ok(())
             }
             26 => {
                 println!("Playing melodic minor scale, press A!");
                 args.scale = Scale::MelodicMinor;
+                Ok(())
             }
-            _ => main_menu(ctx),
+            _ => Err(()),
         }
     }
-    fn octaves_selection(ctx: &mut Ctx, args: &mut ScaleCreatorArgs) {
+    fn octaves_selection(ctx: &mut Ctx, args: &mut ScaleCreatorArgs) -> Result<(), ()> {
         print!("\x1B[2J\x1B[1;1H");
         println!("How many octaves do you want to play?");
         println!("1: A");
@@ -105,98 +145,153 @@ impl ScaleCreator {
         println!("3: C");
         println!("4: D");
         println!("Press any other key to go back to the main menu");
-        let message = get_midi_button(Arc::clone(&ctx.messages));
+        let message = get_midi_down(Arc::clone(&ctx.messages), None);
         match message[1] {
             21 => {
                 args.octaves = 1;
                 println!("Playing 1 octave");
+                Ok(())
             }
             23 => {
                 args.octaves = 2;
                 println!("Playing 2 octaves");
+                Ok(())
             }
             24 => {
                 args.octaves = 3;
                 println!("Playing 3 octaves");
+                Ok(())
             }
             26 => {
                 args.octaves = 4;
                 println!("Playing 4 octaves");
+                Ok(())
             }
-            _ => main_menu(ctx),
+            _ => Err(()),
         }
     }
 
-    fn startkey_selection(ctx: &mut Ctx, args: &mut ScaleCreatorArgs) {
+    fn startkey_selection(ctx: &mut Ctx, args: &mut ScaleCreatorArgs) -> Result<(), ()> {
         println!("Press the key you want to start on");
-        let message = get_midi_button(Arc::clone(&ctx.messages));
+        let message = get_midi_down(Arc::clone(&ctx.messages), None);
         match message[1] {
             21 => {
                 args.offset = 0;
                 println!("Starting on A0");
+                Ok(())
             }
-            23..=111 => {
+            22..=111 => {
                 args.offset = message[1] - 21;
-                println!(
-                    "Starting on {}",
-                    get_note(&(args.offset + 21), KEYS.clone())
-                );
+                println!("Starting on {}", get_note(&(message[1] + 21), KEYS.clone()));
+                Ok(())
             }
-            _ => main_menu(ctx),
+            _ => Err(()),
         }
     }
 
-    fn build_notes(_: &mut Ctx, args: &mut ScaleCreatorArgs) -> Vec<u8> {
-        let mut notes: Vec<u8> = vec![];
-        for i in 0..args.octaves {
-            match args.scale {
-                Scale::Major => {
-                    notes.push((0 + i * 12) + (args.offset + 21));
-                    notes.push((2 + i * 12) + (args.offset + 21));
-                    notes.push((4 + i * 12) + (args.offset + 21));
-                    notes.push((5 + i * 12) + (args.offset + 21));
-                    notes.push((7 + i * 12) + (args.offset + 21));
-                    notes.push((9 + i * 12) + (args.offset + 21));
-                    notes.push((11 + i * 12) + (args.offset + 21));
-                    notes.push((12 + i * 12) + (args.offset + 21));
-                }
-                Scale::NaturalMinor => {
-                    notes.push((0 + i * 12) + (args.offset + 21));
-                    notes.push((2 + i * 12) + (args.offset + 21));
-                    notes.push((3 + i * 12) + (args.offset + 21));
-                    notes.push((5 + i * 12) + (args.offset + 21));
-                    notes.push((7 + i * 12) + (args.offset + 21));
-                    notes.push((8 + i * 12) + (args.offset + 21));
-                    notes.push((10 + i * 12) + (args.offset + 21));
-                    notes.push((12 + i * 12) + (args.offset + 21));
-                }
-                Scale::HarmonicMinor => {
-                    notes.push((0 + i * 12) + (args.offset + 21));
-                    notes.push((2 + i * 12) + (args.offset + 21));
-                    notes.push((3 + i * 12) + (args.offset + 21));
-                    notes.push((5 + i * 12) + (args.offset + 21));
-                    notes.push((7 + i * 12) + (args.offset + 21));
-                    notes.push((8 + i * 12) + (args.offset + 21));
-                    notes.push((11 + i * 12) + (args.offset + 21));
-                    notes.push((12 + i * 12) + (args.offset + 21));
-                }
-                Scale::MelodicMinor => {
-                    notes.push((0 + i * 12) + (args.offset + 21));
-                    notes.push((2 + i * 12) + (args.offset + 21));
-                    notes.push((3 + i * 12) + (args.offset + 21));
-                    notes.push((5 + i * 12) + (args.offset + 21));
-                    notes.push((7 + i * 12) + (args.offset + 21));
-                    notes.push((9 + i * 12) + (args.offset + 21));
-                    notes.push((11 + i * 12) + (args.offset + 21));
-                    notes.push((12 + i * 12) + (args.offset + 21));
-                }
+    fn build_scales_up(scale: &[u8], octave: &u8, offset: &u8) -> Vec<MidiNote> {
+        let mut notes: Vec<MidiNote> = vec![];
+        for i in 0..*octave {
+            for note in scale {
+                notes.push(MidiNote {
+                    _stamp: 0,
+                    message: vec![0, (note + i * 12) + (offset + 21), 0],
+                });
+            }
+        }
+        notes
+    }
+
+    fn build_scales_down(scale: &[u8], octave: &u8, offset: &u8) -> Vec<MidiNote> {
+        let mut notes: Vec<MidiNote> = vec![];
+        for i in (0..*octave).rev() {
+            for note in scale.iter().rev() {
+                notes.push(MidiNote {
+                    _stamp: 0,
+                    message: vec![0, (note + i * 12) + (offset + 21), 0],
+                });
+            }
+        }
+        notes
+    }
+
+    fn build_notes(_: &mut Ctx, args: &mut ScaleCreatorArgs) -> Vec<MidiNote> {
+        let mut notes: Vec<MidiNote> = vec![];
+        match args.scale {
+            Scale::Major => {
+                notes.extend(Self::build_scales_up(
+                    MAJOR_SCALE,
+                    &args.octaves,
+                    &args.offset,
+                ));
+                notes.push(MidiNote {
+                    _stamp: 0,
+                    message: vec![0, (12 + (args.octaves - 1) * 12) + (args.offset + 21), 0],
+                });
+                notes.extend(Self::build_scales_down(
+                    MAJOR_SCALE,
+                    &args.octaves,
+                    &args.offset,
+                ));
+            }
+            Scale::NaturalMinor => {
+                notes.extend(Self::build_scales_up(
+                    NATURAL_MINOR_SCALE,
+                    &args.octaves,
+                    &args.offset,
+                ));
+                notes.push(MidiNote {
+                    _stamp: 0,
+                    message: vec![0, (12 + (args.octaves - 1) * 12) + (args.offset + 21), 0],
+                });
+                notes.extend(Self::build_scales_down(
+                    NATURAL_MINOR_SCALE,
+                    &args.octaves,
+                    &args.offset,
+                ));
+            }
+            Scale::HarmonicMinor => {
+                notes.extend(Self::build_scales_up(
+                    HARMONIC_MINOR_SCALE,
+                    &args.octaves,
+                    &args.offset,
+                ));
+                notes.push(MidiNote {
+                    _stamp: 0,
+                    message: vec![0, (12 + (args.octaves - 1) * 12) + (args.offset + 21), 0],
+                });
+                notes.extend(Self::build_scales_down(
+                    HARMONIC_MINOR_SCALE,
+                    &args.octaves,
+                    &args.offset,
+                ));
+            }
+            Scale::MelodicMinor => {
+                notes.extend(Self::build_scales_up(
+                    MELODIC_MINOR_SCALE,
+                    &args.octaves,
+                    &args.offset,
+                ));
+                notes.push(MidiNote {
+                    _stamp: 0,
+                    message: vec![0, (12 + (args.octaves - 1) * 12) + (args.offset + 21), 0],
+                });
+                notes.extend(Self::build_scales_down(
+                    NATURAL_MINOR_SCALE,
+                    &args.octaves,
+                    &args.offset,
+                ));
             }
         }
         notes
     }
 }
 
-
+#[derive()]
+struct MidiNote {
+    _stamp: u64,
+    message: Vec<u8>,
+}
 
 struct Key {
     _midi: u8,
@@ -208,6 +303,11 @@ impl Key {
         Key { _midi: midi, name }
     }
 }
+
+const MAJOR_SCALE: &'static [u8] = &[0, 2, 4, 5, 7, 9, 11];
+const NATURAL_MINOR_SCALE: &'static [u8] = &[0, 2, 3, 5, 7, 8, 10];
+const HARMONIC_MINOR_SCALE: &'static [u8] = &[0, 2, 3, 5, 7, 8, 11];
+const MELODIC_MINOR_SCALE: &'static [u8] = &[0, 2, 3, 5, 7, 9, 11];
 
 const KEYS: &'static [&'static Key] = &[
     &Key::new(0, "C"),
@@ -224,11 +324,9 @@ const KEYS: &'static [&'static Key] = &[
     &Key::new(11, "B"),
 ];
 
-
 // fn state_enter_playing_scale() {
 //     print!("\x1B[2J\x1B[1;1H");
 // }
-
 
 fn get_note(midikey: &u8, _key: &[&Key]) -> String {
     // println!("Midikey: {}", midikey);
--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,22 @@ pub struct ScaleTrainer {}
 
 unsafe impl Send for ScaleTrainer {}
 
-fn get_midi_button(messages: Arc<Mutex<Vec<u8>>>) -> Vec<u8> {
+fn get_midi_down(messages: Arc<Mutex<Vec<u8>>>,_key: Option<u8>) -> Vec<u8> {
     loop {
         let mut messages = messages.lock().unwrap();
         if messages[0] == 0 || messages[0] == 128 {
+            continue;
+        }
+        // pop message
+        let message = messages.clone();
+        *messages = vec![0, 0, 0];
+        return message;
+    }
+}
+fn get_midi_button(messages: Arc<Mutex<Vec<u8>>>) -> Vec<u8> {
+    loop {
+        let mut messages = messages.lock().unwrap();
+        if messages[0] == 0 {
             continue;
         }
         // pop message
@@ -38,34 +50,42 @@ impl ScaleTrainer {
     }
     fn run(&mut self) {
         let in_port = get_midi_input();
+        match in_port {
+            Ok(_) => (),
+            Err(_) => {
+                println!("No midi input found");
+                return;
+            }
+        }
         let midi_in = MidiInput::new("midir reading input").unwrap();
-        let mut ctx = Ctx {
+        let ctx = Ctx {
             messages: Arc::new(Mutex::new(vec![0, 0, 0])), 
         };
-        let messagei = Arc::clone(&ctx.messages);
+        let message_arc = Arc::clone(&ctx.messages);
         let _conn_in = midi_in.connect(
             &in_port.unwrap(),
             "midir-read-input",
             move |_stamp, message, _| {
-                let mut messages = messagei.lock().unwrap();
+                let mut messages = message_arc.lock().unwrap();
                 *messages = message.clone().to_vec();
                 // println!("{}: {:?} (len = {})", stamp, message, message.len());
             },
             (),
         );
 
-        // let state = Box::new(MainMenuState);
-        //Keep scope alive
-        main_menu(&mut ctx);
+        main_menu(ctx);
     }
 }
 
-fn main_menu(ctx: &mut Ctx) {
+fn main_menu(mut ctx: Ctx) {
     loop {
-        let message = get_midi_button(Arc::clone(&ctx.messages));
+        // print!("\x1B[2J\x1B[1;1H");
+        println!("Main Menu");
+        println!("1. Free Scales: Press A0 to start");
+        let message = get_midi_down(Arc::clone(&ctx.messages),None);
         match message[1] {
-            21 => FreeScale::run(ctx),
-            _ => main_menu(ctx),
+            21 => FreeScale::run(&mut ctx),
+            _ => continue,
         }
     }
 }
@@ -78,7 +98,17 @@ fn get_midi_input() -> Result<MidiInputPort, Box<dyn std::error::Error>> {
     // Get an input port (read from console if multiple are available)
     let in_ports = midi_in.ports();
     let in_port = match in_ports.len() {
-        0 => return Err("no input port found".into()),
+        0 => {
+            let mut input = String::new();
+            println!("Could not find any input port");
+            println!("Please connect your midi, you might need to restart the App!");
+            println!("Press 'r' to retry or any other key to exit: ");
+            stdin().read_line(&mut input)?;
+            match input.trim(){
+                "r" => return get_midi_input(),
+                _ => return Err("no input port found".into()),
+            }
+        }
         1 => {
             println!(
                 "Choosing the only available input port: {}",
@@ -90,6 +120,9 @@ fn get_midi_input() -> Result<MidiInputPort, Box<dyn std::error::Error>> {
             println!("\nAvailable input ports:");
             for (i, p) in in_ports.iter().enumerate() {
                 println!("{}: {}", i, midi_in.port_name(p).unwrap());
+                if midi_in.port_name(p).unwrap().contains("Nord Stage") {
+                    return Ok(p.clone());
+                }
             }
             print!("Please select input port: ");
             stdout().flush()?;
